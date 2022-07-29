d5a253951628a36e274f48eda749c6399b0b2cae
jinzhehao <jinzhehao@souche.com>
2022-07-29 17:26:29 +0800

    fix: sql fix

--- blog/internal/model/sql/comment.sql
+++ blog/internal/model/sql/comment.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS `comment` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'primary key' PRIMARY KEY,
   `user_id` BIGINT NOT NULL COMMENT '关联 user',
   `article_id` BIGINT NOT NULL COMMENT '关联 article',
   `content` LONGTEXT NOT NULL COMMENT '内容',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted_at` DATETIME DEFAULT NULL COMMENT '删除时间',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES user(`id`),
-  FOREIGN KEY (`article_id`) REFERENCES article(`id`)
+  CONSTRAINT fk_comment_user FOREIGN KEY (`user_id`) REFERENCES user(`id`),
+  CONSTRAINT fk_comment_article FOREIGN KEY (`article_id`) REFERENCES article(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '评论表';
